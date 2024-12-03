a50ef24ad845fa5322e73730b353385b2bf205d3
John (Andy) Ryan <john.ryan@cardinalhealth.com>
2024-12-02 21:43:51 -0500

    day 1

---
+++ src/day1/historian_hysteria.py
@@ -0,0 +1,50 @@
+def get_total_distance(input_data):
+    list1 = []
+    list2 = []
+    for input_line in input_data:
+        left, right = input_line.split("   ")
+        list1.append(int(left))
+        list2.append(int(right))
+    list1.sort()
+    list2.sort()
+    running_difference = 0
+    for i in range(len(list1)):
+        running_difference = running_difference + abs(list1[i] - list2[i])
+    return running_difference
+
+def get_similarity_score(input_data):
+    left_numbers = []
+    right_number_map = {}
+    for input_line in input_data:
+        left, right = input_line.split("   ")
+        left_numbers.append(int(left))
+        right_value = int(right)
+        if right_value in right_number_map:
+            right_number_map[right_value] += 1
+        else:
+            right_number_map[right_value] = 1
+    get_similarity_score = 0
+    for value in left_numbers:
+        if value in right_number_map:
+            get_similarity_score += value * right_number_map[value]
+    return get_similarity_score
+
+def read_lines_from_file(file_name: str):
+    with open(file_name, "r") as text_file:
+        return [x.strip() for x in text_file.readlines()]
+
+def get_total_distance_from_file(input_file):
+    input_data = read_lines_from_file(input_file)
+    return get_total_distance(input_data)
+
+def get_similarity_score_from_file(input_file):
+    input_data = read_lines_from_file(input_file)
+    return get_similarity_score(input_data)
+
+if __name__ == "__main__":
+    part1_result = get_total_distance_from_file("data.txt")
+    print(f"part 1 result {part1_result}")
+
+    part2_result = get_similarity_score_from_file("data.txt")
+    print(f"part 2 result {part2_result}")
+
---
+++ tests/day1/historian_hysteria.py
@@ -0,0 +1,30 @@
+import pytest
+from src.day1.historian_hysteria import get_total_distance, get_total_distance_from_file, get_similarity_score, get_similarity_score_from_file
+
+def test_part_1_sample():
+    sample_1_data = [
+        "3   4",
+        "4   3",
+        "2   5",
+        "1   3",
+        "3   9",
+        "3   3"
+    ]
+    assert get_total_distance(sample_1_data) == 11
+
+def test_part_1_sample_file():
+    assert get_total_distance_from_file("data_sample_part1.txt") == 11
+
+def test_part_2_sample():
+    sample_1_data = [
+        "3   4",
+        "4   3",
+        "2   5",
+        "1   3",
+        "3   9",
+        "3   3"
+    ]
+    assert get_similarity_score(sample_1_data) == 31
+
+def test_part_2_sample_file():
+    assert get_similarity_score_from_file("data_sample_part1.txt") == 31
