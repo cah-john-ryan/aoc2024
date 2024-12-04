86818ea920c550c77cf3140d21d0b00e18909190
John (Andy) Ryan <john.ryan@cardinalhealth.com>
2024-12-03 22:30:05 -0500

    day 2 part 2 done

--- src/day2/red_nosed_reports.py
+++ src/day2/red_nosed_reports.py
@@ -1,36 +1,72 @@
 from enum import Enum
+from typing import List
 
 from src.utilities.file_processing import read_lines_from_file
-
-def get_count_of_safe_reports(input_data):
-    count_of_safe_reports = 0
-    for input_line in input_data:
-        if is_report_safe(input_line):
-            count_of_safe_reports += 1
-    return count_of_safe_reports
 
 class ReportDirection(Enum):
     INCREASING = 0
     DECREASING = 1
 
-def is_report_safe(input_line):
-    report_levels = [int(x) for x in input_line.split(' ')]
+def get_count_of_safe_reports(input_data: List[str], use_dampener: bool) -> int:
+    count_of_safe_reports = 0
+    for report_line in input_data:
+        if is_report_line_safe(report_line, use_dampener):
+            print(f"{report_line}: Safe")
+            count_of_safe_reports += 1
+        else:
+            print(f"{report_line}: Unsafe")
+    return count_of_safe_reports
+
+def is_report_line_safe(report_line: str, use_dampener: bool) -> bool:
+    report_levels = [int(x) for x in report_line.split(' ')]
+    if is_report_safe(report_levels):
+        return True
+    else:
+        if use_dampener and is_there_a_shortened_report_that_is_safe(report_levels):
+            return True
+        else:
+            return False
+
+def is_report_safe(report_levels: List[int]) -> bool:
     direction = ReportDirection.DECREASING if report_levels[0] > report_levels[-1] else ReportDirection.INCREASING
     previous_level = report_levels[0]
-    for report_level in report_levels[1:]:
-        if abs(previous_level - report_level) < 1 or 3 < abs(previous_level - report_level):
+    if not is_first_level_safe(report_levels):
+        return False
+    for index, current_level in enumerate(report_levels[1:]):
+        if is_level_safe(direction, previous_level, current_level):
+            previous_level = current_level
+        else:
             return False
-        if direction == ReportDirection.INCREASING and previous_level > report_level:
-            return False
-        elif direction == ReportDirection.DECREASING and previous_level < report_level:
-            return False
-        previous_level = report_level
     return True
 
-def get_count_of_safe_reports_from_file(file_name):
+def is_there_a_shortened_report_that_is_safe(report_levels: List[int]) -> bool:
+    for index, value in enumerate(report_levels):
+        revised_report_levels = report_levels[:]
+        del revised_report_levels[index]
+        if is_report_safe(revised_report_levels):
+            return True
+    return False
+
+def is_first_level_safe(report_levels: List[int]) -> bool:
+    direction_of_2nd_and_3rd_levels = ReportDirection.DECREASING if report_levels[1] > report_levels[2] else ReportDirection.INCREASING
+    return is_level_safe(direction_of_2nd_and_3rd_levels, report_levels[0], report_levels[1])
+
+def is_level_safe(direction: ReportDirection, previous_level: int, current_level: int) -> bool:
+    if abs(previous_level - current_level) < 1 or 3 < abs(previous_level - current_level):
+        return False
+    if direction == ReportDirection.INCREASING and previous_level > current_level:
+        return False
+    elif direction == ReportDirection.DECREASING and previous_level < current_level:
+        return False
+    else:
+        return True
+
+def get_count_of_safe_reports_from_file(file_name: str, use_dampener: bool):
     input_data = read_lines_from_file(file_name)
-    return get_count_of_safe_reports(input_data)
+    return get_count_of_safe_reports(input_data, use_dampener)
 
 if __name__ == '__main__':
-    part1_result = get_count_of_safe_reports_from_file('data.txt')
+    part1_result = get_count_of_safe_reports_from_file('data.txt', False)
     print(f"day2 part 1 result: {part1_result}")
+    part2_result = get_count_of_safe_reports_from_file('data.txt', True)
+    print(f"day2 part 2 result: {part2_result}")
--- tests/day2/red_nosed_reports.py
+++ tests/day2/red_nosed_reports.py
@@ -1,4 +1,4 @@
-from src.day2.red_nosed_reports import get_count_of_safe_reports, get_count_of_safe_reports_from_file
+from src.day2.red_nosed_reports import get_count_of_safe_reports, get_count_of_safe_reports_from_file, is_report_line_safe
 
 def test_part1_sample_data():
     sample_data = [
@@ -10,11 +10,28 @@ def test_part1_sample_data():
         "1 3 6 7 9"
     ]
 
-    result = get_count_of_safe_reports(sample_data)
+    result = get_count_of_safe_reports(sample_data, False)
 
     assert result == 2
 
 def test_part1_sample_data_from_file():
-    result = get_count_of_safe_reports_from_file("data_sample.txt")
+    result = get_count_of_safe_reports_from_file("data_sample.txt", False)
 
     assert result == 2
+
+def test_part2_is_report_safe():
+    report_line = "1 5 6 7 8"
+    result = is_report_line_safe(report_line, True)
+
+    assert result == True
+
+def test_part2_is_report_safe_another_flaw():
+    report_line = "1 3 2 4 5"
+    result = is_report_line_safe(report_line, True)
+
+    assert result == True
+
+def test_part2_sample_data_from_file():
+    result = get_count_of_safe_reports_from_file("data_sample.txt", True)
+
+    assert result == 4
